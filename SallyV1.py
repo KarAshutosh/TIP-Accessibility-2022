40f05f04cfba084756f7e779ea71c78f29bfc326
Ashutosh K <81259418+KarAshutosh@users.noreply.github.com>
2022-11-22 16:59:49 +0530

    Update SallyV1.py

--- SallyV1.py
+++ SallyV1.py
@@ -7,40 +7,66 @@ engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
-
 def talk(text):
     engine.say(text)
     engine.runAndWait()
+
+
+def wait_sally():
+    
+    try:
+        with sr.Microphone() as source:
+            print('Listening...')
+            
+            voice = listener.listen(source)              
+            
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            
+    except:
+        pass
+    return command
 
 
 def take_command():
     
     try:
         with sr.Microphone() as source:
-            print('listening...')
-            voice = listener.listen(source)
+            print('Waiting for command...')
+            
+            voice = listener.listen(source)              
+            
             command = listener.recognize_google(voice)
             command = command.lower()
-#             if 'sally' in command:
-#                 command = command.replace('sally', 'sally')
-#                 print(command)
+            if 'sally' in command:
+                 command = command.replace('sally', '')
+                 print(command)
     except:
         pass
     return command
 
+
 def run_sally():
-    command = take_command()
+    command = wait_sally()
 
     print(command)
 
     if 'omega' in command:
         talk('Hello, please give me a command')
         
+        event = take_command()
+        
+        print(event)
+            
+        if 'list' and 'experiments' in event:
+            talk('The experiments are still under development')        
+        
     elif 'who' and 'are' and 'you' in command:
-        talk('I am Omega')
+        talk('I am Omega. I am developed by T I P accessible labs of bits Goa')
         
     else:
         talk('Please say something')
 
-while True:
+        
+while (1 == 1):
     run_sally()
