0f361272c709897883792d29dd4b2457e230774d
KarAshutosh <karanamashutosh@gmail.com>
2024-03-07 21:52:41 +0530

    New commit

---
+++ colour-detection-2024/0.4_faster_cam.py
@@ -0,0 +1,11 @@
+# pip install picamera
+import picamera
+
+# Create a Picamera instance
+with picamera.PiCamera() as camera:
+    # Adjust camera settings if needed
+    camera.vflip = True  # Vertical flip
+    camera.hflip = True  # Horizontal flip
+    
+    # Capture an image quickly (with minimal delay)
+    camera.capture('test2.jpg')
